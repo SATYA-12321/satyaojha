c61b07230c5d80e01f1ac8605905522335ba8ffa
Satya <satyaprakashjh7@gmail.com>
2026-02-12 18:20:39 +0530

    asdfghjkl

---
+++ main.tf
@@ -0,0 +1,14 @@
+resource "azurerm_resource_group" "satya_rg" {
+  name     = "satya-rg"
+  location = "Central India"
+}
+
+resource "azurerm_storage_account" "satya_storage" {
+  name                     = "satyastorage976496"
+  resource_group_name      = azurerm_resource_group.satya_rg.name
+  location                 = azurerm_resource_group.satya_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+
+}
