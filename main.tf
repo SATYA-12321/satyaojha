4cdd33abeb8e34d078348560a3c3c3d75bc7dc57
Satya Prakash Ojha <satyaprakashjh7@gmail.com>
2026-02-13 18:56:47 +0530

    Rename storage account to 'rolex-'

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "satya_rg" {
 }
 
 resource "azurerm_storage_account" "satya_storage" {
-  name                     = "satyastorage976496"
+  name                     = "rolex-"
   resource_group_name      = azurerm_resource_group.satya_rg.name
   location                 = azurerm_resource_group.satya_rg.location
   account_tier             = "Standard"
