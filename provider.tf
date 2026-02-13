3befd099a8d336925ae00df09709ae0bdbc79c45
Satya Prakash Ojha <satyaprakashjh7@gmail.com>
2026-02-13 18:49:22 +0530

    Fix formatting in provider.tf

--- provider.tf
+++ provider.tf
@@ -1,15 +1,19 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "4.59.0"
     }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "satya-rg"
+    storage_account_name = "satyastorage976496"
+    container_name       = "satya-container"
+    key                  = "terraform.tfstate"
   }
 }
 
 provider "azurerm" {
-
-    features {}
-    
-    
+  features {}
 }
