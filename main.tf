3ffb179570c52ea49c6311dcc62c70d71d6485ab
Satya Prakash Ojha <satyaprakashjh7@gmail.com>
2026-02-14 08:49:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,14 +3,13 @@ resource "azurerm_resource_group" "satya_rg" {
   location = "Central India"
 }
 
-resource "azurerm_storage_account" "satya_storage" {
-  name                     = "rolex-"
-  resource_group_name      = azurerm_resource_group.satya_rg.name
-  location                 = azurerm_resource_group.satya_rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+#resource "azurerm_storage_account" "satya_storage" {
+ # resource_group_name      = azurerm_resource_group.satya_rg.name
+ # location                 = azurerm_resource_group.satya_rg.location
+ # account_tier             = "Standard"
+ # account_replication_type = "LRS"
 
 
-}
+#}
 
 # satya ojha nahi kar sakte ha
